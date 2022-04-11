7d509b655ea47921c8156299883a628e79f17140
Craig McGowan <craigmcgowan15@gmail.com>
2022-04-10 20:34:11 -0400

    initial nav-bar set up

---
+++ src/components/Header.svelte
@@ -0,0 +1,16 @@
+     <script>
+       import { goto } from "$app/navigation";
+     </script>
+     
+     <header>
+        <div >
+            Craig
+            McGowan
+        </div>
+        <nav>
+          <li class="list-none" on:click= {()=>goto("/")}>About</li>
+          <li class="list-none"  on:click= {()=>goto("/contact")}>Contact</li>
+          <li class="list-none"  on:click= {()=>goto("/projects")}>Projects</li>
+  
+        </nav>
+      </header>
