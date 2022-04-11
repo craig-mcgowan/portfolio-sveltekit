0426c25911c11a377d547e212e9ab6325142fc8c
Craig McGowan <craigmcgowan15@gmail.com>
2022-04-10 21:09:16 -0400

    initial about page set up

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,13 @@
-<h3>Hey, I'm Craig!</h3>
-<img src="https://res.cloudinary.com/dm9ig1vtk/image/upload/v1643915636/Portfolio/20210112_205301-removebg-preview_lwyszl.png" alt="Craig McGowan"/>
-<p>I'm a full-stack developer committed to building intuitive, performative and beautiful web applications. In my free time I enjoy hiking, exploring and playing Ultimate.</p>
+<script>
+  import { fade } from "svelte/transition";
+</script>
+
+<div in:fade>
+
+  <h3>Hey, I'm Craig!</h3>
+  <img  class="rounded-2xl" src="https://res.cloudinary.com/dm9ig1vtk/image/upload/v1643915636/Portfolio/20210112_205301-removebg-preview_lwyszl.png" alt="Craig McGowan"/>
+  <div>
+    <p>I'm a full-stack developer committed to building intuitive, performative and beautiful web applications.</p> 
+    <p>In my free time I enjoy hiking, exploring, and playing Ultimate Frisbee.</p>
+  </div>
+</div>
