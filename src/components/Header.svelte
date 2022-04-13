976c1c1037f5a70e5729ed0bc1bfb579b2a7fc96
Craig McGowan <craigmcgowan15@gmail.com>
2022-04-12 22:13:48 -0400

    styling

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,14 +2,14 @@
        import { goto } from "$app/navigation";
      </script>
      
-     <header class= "flex w-full justify-between bg-black text-white">
+     <header class= "flex w-full justify-between bg-black text-white h-12">
         <div class = "p-2">
             Craig
             McGowan
         </div>
         <nav class="flex justify-around w-max">
-          <li class="list-none px-4 py-2" on:click= {()=>goto("/")}>About</li>
-          <li class="list-none px-4 py-2"  on:click= {()=>goto("/contact")}>Contact</li>
-          <li class="list-none px-4 py-2"  on:click= {()=>goto("/projects")}>Projects</li>
+          <li class="list-none px-4 py-2 hover:text-lg cursor-pointer" on:click= {()=>goto("/")}>About</li>
+          <li class="list-none px-4 py-2 hover:text-lg cursor-pointer"  on:click= {()=>goto("/contact")}>Contact</li>
+          <li class="list-none px-4 py-2 hover:text-lg cursor-pointer"  on:click= {()=>goto("/projects")}>Projects</li>
         </nav>
       </header>
