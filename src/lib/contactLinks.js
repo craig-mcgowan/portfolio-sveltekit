3ec7380e6be9249dcb43542279c2a343f31e35b4
Craig McGowan <craigmcgowan15@gmail.com>
2022-04-14 21:13:49 -0400

    contact page

---
+++ src/lib/contactLinks.js
@@ -0,0 +1,9 @@
+import DiGithubFull from 'svelte-icons/di/DiGithubFull.svelte'
+import FaLinkedinIn from 'svelte-icons/fa/FaLinkedinIn.svelte';
+import FaTwitter from 'svelte-icons/fa/FaTwitter.svelte'
+
+export const contactLinks = [
+	{ site: 'linkedIn', url: 'https://www.linkedin.com/in/cmcgowan/', icon: DiGithubFull },
+	{ site: 'GitHub', url: 'https://github.com/craig-mcgowan', icon: FaLinkedinIn },
+	{ site: 'Twitter', url: 'https://twitter.com/craigmmcgowan', icon: FaTwitter  }
+];
---
+++ src/routes/contact.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { contactLinks } from "$lib/contactLinks";
+</script>
+
+{#each contactLinks as contactLink}
+
+  <div class="flex flex-col items-center">
+    <img class="rounded-2xl" src="{contactLink.image}" alt="{contactLink.name}"/>
+    <div>
+      <h3>{contactLink.name}</h3>
+      <p>{contactLink.description}</p>
+      <a href="{contactLink.link}">{contactLink.link}</a>
+    </div>
+  </div>
+
+{/each}
