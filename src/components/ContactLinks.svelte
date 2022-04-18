cc01e4a39c8ea7e7022e264712bad00b9186e5cc
Craig McGowan <craigmcgowan15@gmail.com>
2022-04-17 23:33:57 -0400

    styles social links

--- src/components/ContactLinks.svelte
+++ src/components/ContactLinks.svelte
@@ -2,17 +2,18 @@
   import {contactLinks} from "$lib/contactLinks";
 </script>
 
-{#each contactLinks as {site, url, icon} }
+<div class="flex w-max">
 
-  <div class="flex flex-col items-center">
+  {#each contactLinks as {site, url, icon} }
+  
+  <div >
     <a href={url} target=_blank >
       <div class= "h-16 flex-grow-0 my-2  p-2 rounded-2xl min-w-fit duration-200 ease-linear transition-all" >
         <svelte:component this={icon} class="h-10"/>
       </div>
-      <div>
-        <h3>{site}</h3>
-      </div>
+
     </a>
   </div>
-
-{/each}
+  
+  {/each}
+</div>
