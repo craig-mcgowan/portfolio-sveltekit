21a216d243efd3feae01e3146517fbe3ebe7f128
Craig McGowan <craigmcgowan15@gmail.com>
2022-04-17 23:31:25 -0400

    contact styling

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -3,6 +3,9 @@
   import ContactLinks from "../components/ContactLinks.svelte";
   import ContactForm from "../components/ContactForm.svelte";
 </script>
-
-<ContactLinks/>
-<ContactForm/>
+<div class="flex flex-col content-center items-center">
+  <div class = "mb-4">
+    <ContactLinks/>
+  </div>
+  <ContactForm/>
+</div>
